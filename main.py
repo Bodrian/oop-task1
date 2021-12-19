f76b374ac821fb6a791915966c953fb838654731
Bodrian <u3363777@yandex.ru>
2021-12-19 11:29:49 +0300

    task 2

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lect(self, lector, course, grade):
+        if isinstance(lector, Lecturer) and course in self.courses_in_progress and course in lector.courses_attached:
+            if course in lector.grades:
+                lector.grades[course] += [grade]
+            else:
+                lector.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,6 +22,14 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        self.grades = {}
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -23,22 +39,19 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
-
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_lecturer = Lecturer('Some', 'Buddy')
+cool_reviewer = Reviewer('Some1', 'Buddy1')
+cool_lecturer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+best_student.rate_lect(cool_lecturer, 'Python', 10)
+#cool_mentor.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+print(cool_lecturer.grades)
